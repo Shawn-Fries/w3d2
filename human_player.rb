99677b64e81329ca5c92588e64becafbedec82e5
Shawn-Fries <samfoley275@gmail.com>
2021-12-14 16:38:13 -0500

    add human player

--- human_player.rb
+++ human_player.rb
@@ -1,3 +1,8 @@
 class HumanPlayer
-    
+    def prompt
+        p 'Please input a position to check, separated by a space'
+        pos = gets.chomp.split(' ').map {|ele| ele.to_i}
+    end
+
+    def 
 end
